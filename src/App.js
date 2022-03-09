fdcea3c04c53c5120956d0d93b405e1e09cfb1e6
sungalex <cloudallera@gmail.com>
2022-03-10 00:15:06 +0900

    as and attrs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 const Box = styled.div`
   background-color: ${(props) => props.backgroundColor};
   width: 100px;
   height: 100px;
 `;
-
 const Circle = styled(Box)`
   border-radius: 50px;
+`;
+const Button = styled.button`
+  background-color: tomato;
+  color: white;
+  border-radius: 15px;
+`;
+const Input = styled.input.attrs({ required: true })`
+  background-color: teal;
 `;
 
 function App() {
@@ -18,6 +26,13 @@ function App() {
     <Father>
       <Box backgroundColor="teal" />
       <Circle backgroundColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Button>Log in</Button>
+      <Button as="a" href="/">
+        Link
+      </Button>
     </Father>
   );
 }
