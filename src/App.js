78cd00eb8305376ed412916595c600d9fb96ea48
sungalex <cloudallera@gmail.com>
2022-03-09 23:56:10 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -3,27 +3,21 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.backgroundColor};
   width: 100px;
   height: 100px;
 `;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.span`
-  color: white;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box backgroundColor="teal" />
+      <Circle backgroundColor="tomato" />
     </Father>
   );
 }
