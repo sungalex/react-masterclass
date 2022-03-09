5af7f22cb74b24f60c3a62454181470ef8a81a57
sungalex <cloudallera@gmail.com>
2022-03-10 00:56:48 +0900

    Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
+`;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 const Box = styled.div`
-  background-color: ${(props) => props.backgroundColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-const Button = styled.button`
   background-color: tomato;
-  color: white;
-  border-radius: 15px;
-`;
-const Input = styled.input.attrs({ required: true })`
-  background-color: teal;
+  width: 200px;
+  height: 200px;
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 72px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Box backgroundColor="teal" />
-      <Circle backgroundColor="tomato" />
-      <Input />
-      <Input />
-      <Input />
-      <Button>Log in</Button>
-      <Button as="a" href="/">
-        Link
-      </Button>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🤩</span>
+      </Box>
+    </Wrapper>
   );
 }
 
