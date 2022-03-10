ae1169079d7b38d1b18a345dd42ae202ec7a66a0
sungalex <cloudallera@gmail.com>
2022-03-11 00:06:08 +0900

    Typing the Props with interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-const theme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
