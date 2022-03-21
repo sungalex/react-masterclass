421cc404c77fff6979c1c3285cfcb4c399921e85
sungalex <cloudallera@gmail.com>
2022-03-22 00:40:47 +0900

    To Do Setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,31 @@
+import React, { useState } from "react";
+
 function ToDoList() {
-  return <div>ToDo</div>;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+    setToDo("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
