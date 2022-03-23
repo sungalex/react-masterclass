bdd17d70c89030ff39b542c2a73e2c93f59f5422
sungalex <cloudallera@gmail.com>
2022-03-23 19:05:48 +0900

    Go to back toto_app with react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,128 +1,23 @@
-/*
-import React, { useState } from "react";
-
-function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const [toDoError, setToDoError] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDoError("");
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (toDo.length < 10) {
-      setToDoError("To do should be longer");
-    } else {
-      console.log("submit:", toDo);
-      setToDo("");
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-        <span>{toDoError !== "" ? toDoError : null}</span>
-      </form>
-    </div>
-  );
-} */
-
 import { useForm } from "react-hook-form";
 
 interface IForm {
-  email: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string;
 }
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
   const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "Server offline" });
-    console.log(data);
+    console.log("add to do:", data.toDo);
+    setValue("toDo", "");
   };
-  console.log(errors);
-
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onValid)}
-        style={{ display: "flex", flexDirection: "column", padding: "20px" }}>
+      <form onSubmit={handleSubmit(onValid)}>
         <input
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
-          })}
-          placeholder="Email"
+          {...register("toDo", { required: "Please write a To Do" })}
+          placeholder="Write a to do"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", { required: true })}
-          placeholder="First Name"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: true })}
-          placeholder="Last Name"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", {
-            required: "Username is required",
-            validate: {
-              alex: (value) =>
-                value.includes("alex") ? "alex is not allowed" : true,
-              hello: (value) =>
-                value.includes("hello") ? "hello is not allowd" : true,
-            },
-            minLength: {
-              value: 10,
-              message: "Username is too short",
-            },
-          })}
-          placeholder="Username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", { required: "Password is required" })}
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", { required: true })}
-          placeholder="Password1"
-        />
-        <span>{errors?.password1?.message}</span>
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
