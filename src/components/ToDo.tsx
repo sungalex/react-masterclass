ba3208a335f38527fe75e9562756ca57255cde7c
sungalex <cloudallera@gmail.com>
2022-03-24 23:39:24 +0900

    Immutability: toDos Array

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, id, category }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
     console.log("I wanna go to ", name);
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      const newToDos = [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+      return newToDos;
+    });
   };
   return (
     <li>
