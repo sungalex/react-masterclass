55c9a5d732a683953153393db93af8cf539adb5b
sungalex <cloudallera@gmail.com>
2022-03-25 02:08:48 +0900

    seperate Select Option

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,17 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
-import { categoryState, Catetories, toDoSelector } from "../atoms";
+import { toDoSelector } from "../atoms";
 import React from "react";
+import SelectCategory from "./SelectCategory";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
-  const [category, setCategory] = useRecoilState(categoryState);
-  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value as Catetories);
-  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <select value={category} onInput={onInput}>
-        <option value={Catetories.TO_DO}>To Do</option>
-        <option value={Catetories.DOING}>Doing</option>
-        <option value={Catetories.DONE}>Done</option>
-      </select>
+      <SelectCategory />
       <CreateToDo />
       {toDos?.map((toDo) => (
         <ToDo key={toDo.id} {...toDo} />
