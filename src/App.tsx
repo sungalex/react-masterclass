e8fd4b3a915b6a8d1892c0542d1ffd9da9fe1d56
sungalex <cloudallera@gmail.com>
2022-04-04 23:46:00 +0900

    initializing for trello clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { hourState, minuteState } from "./atoms";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourState);
-  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-  const onHourChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-  return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinuteChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        value={hours}
-        onChange={onHourChange}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
-  );
+  return null;
 }
 
 export default App;
