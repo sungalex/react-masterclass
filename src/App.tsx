13ca5fd2c8d584b95e072afc0403b817f7a4d6ef
sungalex <cloudallera@gmail.com>
2022-04-04 22:55:48 +0900

    Recoil Selectors - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourState, minuteState } from "./atoms";
+
 function App() {
-  return null;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourState);
+  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHourChange = () => {};
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinuteChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHourChange}
+        type="number"
+        placeholder="Hours"
+      />
+    </div>
+  );
 }
 
 export default App;
