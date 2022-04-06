722e2eb546ae2d9f68af411dc7b367cead127777
sungalex <cloudallera@gmail.com>
2022-04-06 23:53:04 +0900

    Draggable Snapshot

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin: 5px;
   padding: 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.isDragging ? "tomato" : props.theme.cardColor};
 `;
 
 interface IDragabbleCardProps {
@@ -17,8 +18,9 @@ interface IDragabbleCardProps {
 function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
   return (
     <Draggable draggableId={toDo} key={toDo} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}>
