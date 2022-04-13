f09c2eec71f426aa708f824521b7c50c7cd83e15
sungalex <cloudallera@gmail.com>
2022-04-13 18:15:58 +0900

    Variants part Two - with delayChildren and staggerChildren

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const variants = {
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
   start: {
-    scale: 0,
+    opacity: 0,
+    scale: 0.5,
   },
   end: {
+    opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={variants} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
